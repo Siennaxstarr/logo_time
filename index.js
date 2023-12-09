34cb1e396fb162fc3860feb85c19596be0a15ac5
Siennaxstarr <siennastarrv@gmail.com>
2023-12-08 17:54:26 -0800

    Added color, shape, and text

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const SVG = require('./lib/svg.js');
+const {
+    Circle, Square, Triangle
+} = require(`./lib/shapes.js`);
+const inquirer = require('inquirer');
+
+function start() {
+    inquirer.prompt([
+        {
+            name: "text",
+            type: "input",
+            message: "enter a 3 letter logo"
+        },
+        {
+            type: "input",
+            name: "text_color",
+            message: "what color would you like?"
+        }
+        ,
+        {
+            type: "list",
+            name: "shape",
+            message: "Select a shape",
+            choices: ["circle", "square", "triangle"]
+        },
+        {
+            type: "input",
+            name: "shape_color",
+            message: "what color would you like your shape?"
+        }
+
+    ])
+    .then(function(data){
+        var shape;
+        if(data.shape === "circle"){
+            shape = new Circle()
+        }
+        if(data.shape === "square"){
+            shape = new Square()
+
+        }
+        if(data.shape === "triangle"){
+            shape = new Triangle()
+        }
+      shape.color = data.shape_color  
+      const svg = new SVG ();
+      svg.settext(data.text, data.text_color)
+      return fs.writeFileSync("logo.svg", svg.render())
+
+
+
+    })
+}
+start()
